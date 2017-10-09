b180a5bdfc31be2519322275c9016aeeb64946fe
rogerbusquets97 <duranbusquetsroger@gmail.com>
2017-10-09 17:04:09 +0200

    Evade

---
+++ AI_clas_04/Assets/Steering/SteeringEvade.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SteeringEvade : MonoBehaviour {
+    public float max_prediction;
+
+    Move move;
+    SteeringArrive arrive;
+
+    // Use this for initialization
+    void Start () {
+
+        move = GetComponent<Move>();
+        arrive = GetComponent<SteeringArrive>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Steer(move.target.transform.position, move.target.GetComponent<Move>().movement);
+
+    }
+
+    public void Steer(Vector3 target, Vector3 velocity)
+    {
+        
+        Vector3 prediction = target + velocity * max_prediction;
+        
+        arrive.Steer(-prediction);
+    }
+
+
+}
